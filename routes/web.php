e28a988237a8ccce7e7f1eb17a846c0bbd0f049e
Rodrick Kazembe <a-team@kode-blog.com>
2016-11-29 14:35:44 +0300

    Update point homepage route to HomeController@index, named the route as home

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+   'uses' => '\Larashop\Http\Controllers\HomeController@index',
+   'as' => 'home',
+]);
