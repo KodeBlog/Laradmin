fa80fc58f4f3df2caeb1a035d0694f80c50765dc
Rodrick Kazembe <a-team@kode-blog.com>
2016-12-02 15:34:24 +0300

    Add controllers for categories, customers, dashboard, orders, products and users

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Larashop\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Larashop\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Larashop\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Larashop\Http\Controllers\Controller;
+
+class OrdersController extends Controller
+{
+    public function index(){
+        //
+    }
+}
