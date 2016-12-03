8166a95f7ef7e214405914cf59d5dbc80b4da257
Rodrick Kazembe <a-team@kode-blog.com>
2016-12-03 12:41:23 +0300

    Add product categories controller

---
+++ app/Http/Controllers/Admin/ProductCategoriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Larashop\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Larashop\Http\Controllers\Controller;
+
+class ProductCategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.categories.categories_list');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.categories_create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return redirect()->route('product-categories.index')->with('success', "The product category <strong>Category</strong> has successfully been created.");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('admin.categories.categories_delete');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('admin.categories.categories_edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return redirect()->route('product-categories.index')->with('success', "The product category <strong>Category</strong> has successfully been updated.");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return redirect()->route('product-categories.index')->with('success', "The product category <strong>Category</strong> has successfully been archived.");
+    }
+}
