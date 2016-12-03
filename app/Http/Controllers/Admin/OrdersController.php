50894f52641b114c54ba2a8089dd24828388fd35
Rodrick Kazembe <a-team@kode-blog.com>
2016-12-03 12:46:16 +0300

    Update methods for orders controller to show views and redirect

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -7,7 +7,13 @@ use Larashop\Http\Controllers\Controller;
 
 class OrdersController extends Controller
 {
-    public function index(){
-        //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+        public function index()
+        {
+            return view('admin.orders.orders_list');
+        }
     }
-}
