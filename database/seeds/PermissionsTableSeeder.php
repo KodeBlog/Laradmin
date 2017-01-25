b87a783d08da9c1e30c4809010d1dfaa90ea9cbf
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-25 11:43:41 +0300

    Add permissions seeder

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Larashop\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permission = [
+            [
+                'name' => 'create',
+                'display_name' => 'Create Record',
+                'description' => 'Allow user to create a new DB record'
+            ],
+            [
+                'name' => 'edit',
+                'display_name' => 'Edit Record',
+                'description' => 'Allow user to edit an existing DB record'
+            ],
+            [
+                'name' => 'delete',
+                'display_name' => 'Delete Record',
+                'description' => 'Allow user to delete an existing DB record'
+            ],
+            [
+                'name' => 'users',
+                'display_name' => 'Manage Users',
+                'description' => 'Allow user to manage system users'
+            ]
+        ];
+
+        foreach ($permission as $key => $value) {
+            Permission::create($value);
+        }
+    }
+}
