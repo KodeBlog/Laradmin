36e8813e345469d3ba1b09fe8bf64a57b9df2753
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-26 14:44:48 +0300

    Fix remove empty space in constructor method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return view ('welcome');
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome');
     }
 }
