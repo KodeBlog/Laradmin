b7920be98f6796a114632972d1077582d22687e2
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-26 13:15:50 +0300

    Add custom error handler view

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Permission Denied</title>
+
+    <!-- Bootstrap -->
+    <link href="{{asset('admin/css/bootstrap.min.css')}}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{asset('admin/css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="{{asset('admin/css/nprogress.css')}}" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="{{asset('admin/css/custom.min.css')}}" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <!-- page content -->
+        <div class="col-md-12">
+          <div class="col-middle">
+            <div class="text-center text-center">
+              <h1 class="error-number">403</h1>
+              <h2>Access denied</h2>
+              <p>You do not have permission to access this resource.
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="{{asset('admin/js/jquery.min.js')}}"></script>
+    <!-- Bootstrap -->
+    <script src="{{asset('admin/js/bootstrap.min.js')}}"></script>
+    <!-- FastClick -->
+    <script src="{{asset('admin/js/fastclick.js')}}"></script>
+    <!-- NProgress -->
+    <script src="{{asset('admin/js/nprogress.js')}}"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="{{asset('admin/js/custom.min.js')}}"></script>
+  </body>
+</html>
