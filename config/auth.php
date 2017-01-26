9b1fb61232818ef645742f918c849cd89591da80
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-26 14:57:19 +0300

    Update authentication user model namespace from Larashop to Larashop\Models

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Larashop\User::class,
+            'model' => Larashop\Models\User::class,
         ],
 
         // 'users' => [
