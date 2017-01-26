d197c56e350879572f4cbf7814e952e6ca8ae83a
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-26 13:14:31 +0300

    Add custom email notification

---
+++ app/Notifications/LarashopAdminResetPassword.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Larashop\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class LarashopAdminResetPassword extends Notification
+{
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', route('password.reset.token',['token' => $this->token]))
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+}
