9ddbe339ee6d2b748e0f094fe523e52c97175bbe
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-26 14:43:38 +0300

    Update redirect successful logins to admin/brands route

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin/brands';
 
     /**
      * Create a new controller instance.
