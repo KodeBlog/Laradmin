68888b2ba7ed31bd3bd5a44e43881370dfa20ee4
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-27 17:40:23 +0300

    Format symetrical beauty

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -2,17 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
-    |
-    */
-
     'accepted'             => 'The :attribute must be accepted.',
     'active_url'           => 'The :attribute is not a valid URL.',
     'after'                => 'The :attribute must be a date after :date.',
@@ -96,8 +85,8 @@ return [
     */
 
     'custom' => [
-        'attribute-name' => [
-            'rule-name' => 'custom-message',
+        'attribute-name'    => [
+            'rule-name'     => 'custom-message',
         ],
     ],
 
