cde88566a388fb23c9d3c932c9833d14006ee8ee
Rodrick Kazembe <a-team@kode-blog.com>
2017-01-27 17:41:57 +0300

    Feature localize routes

--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,34 @@
 */
 
 Route::get('/', [
-   'uses' => 'HomeController@index',
-   'as' => 'home',
-   ]);
+    'uses' => 'HomeController@index',
+    'as' => 'home',
+    ]);
 
-Route::group(['prefix' => 'admin','middleware' => 'auth','namespace' => 'Admin'],function(){
-    Route::resource('customers', 'CustomersController');
-    Route::resource('brands', 'BrandsController');
-    Route::resource('product-categories', 'ProductCategoriesController');
-    Route::resource('products', 'ProductsController');
-    Route::resource('users', 'UsersController');
+Route::group(['prefix' => LaravelLocalization::setLocale(),'middleware' => [ 'localeSessionRedirect', 'localizationRedirect' ]], function(){
+    Route::group(['prefix' => 'admin','middleware' => 'auth','namespace' => 'Admin'],function(){
+        Route::resource('customers', 'CustomersController');
+        Route::resource('brands', 'BrandsController');
+        Route::resource('product-categories', 'ProductCategoriesController');
+        Route::resource('products', 'ProductsController');
+        Route::resource('users', 'UsersController');
 
-    Route::get('orders',[
-        'uses' => 'OrdersController@index',
-        'as' => 'orders.index',
-        ]);
+        Route::get('orders',[
+            'uses' => 'OrdersController@index',
+            'as' => 'orders.index',
+            ]);
+    });
+
+    // Authentication Routes...
+    $this->get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
+    $this->post('admin/login', 'Auth\LoginController@login');
+    $this->post('admin/logout', 'Auth\LoginController@logout')->name('logout');
+
+    // Password Reset Routes...
+    $this->get('admin/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+    $this->post('admin/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    $this->get('admin/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset.token');
+    $this->post('admin/password/reset', 'Auth\ResetPasswordController@reset');
 });
-
-//Auth::routes();
-// Authentication Routes...
-$this->get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
-$this->post('admin/login', 'Auth\LoginController@login');
-$this->post('admin/logout', 'Auth\LoginController@logout')->name('logout');
-
-// Password Reset Routes...
-$this->get('admin/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
-$this->post('admin/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-$this->get('admin/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset.token');
-$this->post('admin/password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index');
