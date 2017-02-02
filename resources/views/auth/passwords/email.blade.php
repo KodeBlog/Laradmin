7f6ed9e7d2962e01781289264dd7cbd3bfb8eb03
Rodrick Kazembe <a-team@kode-blog.com>
2017-02-02 18:33:16 +0300

    Format prettify code

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,73 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Larashop Admin Login</title>
+        <title>Larashop Admin Login</title>
 
-    <!-- Bootstrap -->
-    <link href="{{asset('admin/css/bootstrap.min.css')}}" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="{{asset('admin/css/font-awesome.min.css')}}" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="{{asset('admin/css/nprogress.css')}}" rel="stylesheet">
-    <!-- Animate.css -->
-    <link href="{{asset('admin/css/animate.min.css')}}" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="{{asset('admin/css/bootstrap.min.css')}}" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="{{asset('admin/css/font-awesome.min.css')}}" rel="stylesheet">
+        <!-- NProgress -->
+        <link href="{{asset('admin/css/nprogress.css')}}" rel="stylesheet">
+        <!-- Animate.css -->
+        <link href="{{asset('admin/css/animate.min.css')}}" rel="stylesheet">
 
-    <!-- Custom Theme Style -->
-    <link href="{{asset('admin/css/custom.min.css')}}" rel="stylesheet">
-</head>
+        <!-- Custom Theme Style -->
+        <link href="{{asset('admin/css/custom.min.css')}}" rel="stylesheet">
+    </head>
 
-<body class="login">
-    <div>
-        <a class="hiddenanchor" id="signup"></a>
-        <a class="hiddenanchor" id="lostpassword"></a>
+    <body class="login">
+        <div>
+            <a class="hiddenanchor" id="signup"></a>
+            <a class="hiddenanchor" id="lostpassword"></a>
 
-        <div class="login_wrapper">
-            <div class="animate form login_form">
-                <section class="login_content">
-                    @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        <h1>Password Reset</h1>
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            @if ($errors->has('email'))
-                            <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
-                            @endif
-                            <input type="text" class="form-control" id="email" name="email" placeholder="Email"/>
+            <div class="login_wrapper">
+                <div class="animate form login_form">
+                    <section class="login_content">
+                        @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
                         </div>
+                        @endif
 
-                        <div>
-                            <button type="submit" class="btn btn-default submit">Send Password Reset Link</button>
-                            <a class="reset_pass" href="{{route('login')}}">Login</a>
-                        </div>
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+                            <h1>Password Reset</h1>
+                            {{ csrf_field() }}
 
-                        <div class="clearfix"></div>
-
-                        <div class="separator">
-
-                            <div class="clearfix"></div>
-                            <br />
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                @if ($errors->has('email'))
+                                <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+                                @endif
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Email"/>
+                            </div>
 
                             <div>
-                                <h1><i class="fa fa-paw"></i> Larashop Admin Panel</h1>
-                                <p>©2017 All Rights Reserved.</p>
+                                <button type="submit" class="btn btn-default submit">Send Password Reset Link</button>
+                                <a class="reset_pass" href="{{route('login')}}">Login</a>
                             </div>
-                        </div>
-                    </form>
-                </section>
+
+                            <div class="clearfix"></div>
+
+                            <div class="separator">
+
+                                <div class="clearfix"></div>
+                                <br />
+
+                                <div>
+                                    <h1><i class="fa fa-paw"></i> Larashop Admin Panel</h1>
+                                    <p>©2017 All Rights Reserved.</p>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
